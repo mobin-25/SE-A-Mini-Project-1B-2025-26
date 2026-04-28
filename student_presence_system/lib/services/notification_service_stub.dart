08bf19986b33d82d64c057ec23e5b1566489ea97
mobin-25 <mobinshk05@gmail.com>
2026-04-28 16:10:44 +0530

    group 16

--- student_presence_system/lib/services/notification_service_stub.dart
+++
@@ -1,9 +0,0 @@
-// lib/services/notification_service_stub.dart
-
-Future<void> requestWebPermission() async {
-  // No-op on mobile
-}
-
-void showWebNotification({required String title, required String body}) {
-  // No-op on mobile
-}
--- student_presence_system/run_flutter.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "c:\Users\Admin\Desktop\Flutter-Dev\projects\practices\student_presence_system"
-flutter config --no-analytics
-flutter run -d chrome
-pause
